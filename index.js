5e2128af45e92d18c0acbcd0337d2938c7358406
Megan Reardon <meganr@gmail.com>
2016-12-09 15:58:21 -0800

    parsing the pixel array

--- index.js
+++ index.js
@@ -3,5 +3,6 @@
 const bmHelper = require('./lib/bitmap-file-helper.js');
 
 bmHelper.load('./img/palette-bitmap.bmp', function(err, bitMapData) {
+  console.log(bitMapData.colors[28]);
   console.dir(bitMapData);
 });
